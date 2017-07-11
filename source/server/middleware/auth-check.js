b521d6d4b15913a89646bb1776a65c9db1990855
ionkoto-user <ionkoto@abv.bg>
2017-07-11 23:22:26 +0300

    Messenger
    Implemented Messenger App. Includes two new models - message and thread.
    Users are able to choose from existing threads or open new ones.

--- source/server/middleware/auth-check.js
+++ source/server/middleware/auth-check.js
@@ -15,7 +15,6 @@ module.exports = (req, res, next) => {
     if (err) { return res.status(401).end() }
 
     const userId = decoded.sub
-
     User
       .findById(userId)
       .then((user) => {
